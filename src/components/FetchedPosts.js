4afcecc24480acccdb475fdf99d3d7c344c91e10
mihalichpalich <pavlovtfspb@gmail.com>
2020-08-26 16:21:02 +0300

    создал action для загрузки асинхронных постов

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import Post from "./Post";
+import {useDispatch, useSelector} from "react-redux";
 
-export default ({posts}) => {
+import Post from "./Post";
+import {fetchPosts} from "../redux/actions";
+
+export default () => {
+    const dispatch = useDispatch();
+    const posts = useSelector(state => state.posts.fetchedPosts);
+
     if (!posts.length) {
-        return <button className="btn btn-primary">Загрузить</button>
+        return <button className="btn btn-primary" onClick={() => dispatch(fetchPosts())}>Загрузить</button>
     }
 
-    return posts.map(post => <Post post={post} key={post}/>)
+    return posts.map(post => <Post post={post} key={post.id}/>)
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, compose} from "redux";
+import {createStore, compose, applyMiddleware} from "redux";
 import {Provider} from 'react-redux';
+import thunk from "redux-thunk";
 
 import {rootReducer} from "./redux/rootReducer";
 
 const store = createStore(
     rootReducer,
-    compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(
+        applyMiddleware(
+            thunk
+        ),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
 const app = (

12b88a39a8f17904098bbcb6bda0ac359fed0f98
mihalichpalich <pavlovtfspb@gmail.com>
2020-08-26 16:36:53 +0300

    добавил Loader

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -3,10 +3,16 @@ import {useDispatch, useSelector} from "react-redux";
 
 import Post from "./Post";
 import {fetchPosts} from "../redux/actions";
+import {Loader} from "./Loader";
 
 export default () => {
     const dispatch = useDispatch();
     const posts = useSelector(state => state.posts.fetchedPosts);
+    const loading = useSelector(state => state.app.loading);
+
+    if (loading) {
+        return <Loader/>
+    }
 
     if (!posts.length) {
         return <button className="btn btn-primary" onClick={() => dispatch(fetchPosts())}>Загрузить</button>
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import {CREATE_POST, FETCH_POSTS} from "./types";
+import {CREATE_POST, FETCH_POSTS, HIDE_LOADER, SHOW_LOADER} from "./types";
 
 export const createPost = (post) => {
     return {
@@ -7,14 +7,32 @@ export const createPost = (post) => {
     }
 };
 
+export const showLoader = () => {
+    return {
+        type: SHOW_LOADER
+    }
+};
+
+export const hideLoader = () => {
+    return {
+        type: HIDE_LOADER
+    }
+};
+
 export const fetchPosts = () => {
     return async dispatch => {
+        dispatch(showLoader());
+
         const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
         const postsJSON = await response.json();
 
-        dispatch({
-            type: FETCH_POSTS,
-            payload: postsJSON
-        })
+        setTimeout(() => {
+            dispatch({
+                type: FETCH_POSTS,
+                payload: postsJSON
+            });
+            dispatch(hideLoader())
+        }, 500);
+
     }
 };
